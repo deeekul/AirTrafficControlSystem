1a797c384b6a45640dc9e2b13e2855ec3f3d21d0
Dmitry Kulinchenko <dima.kul22@mail.ru>
2023-05-06 22:55:29 +0300

    add PlaneDTO

--- src/main/java/ru/vsu/cs/airTrafficControlSystem/dto/PlaneDTO.java
+++ src/main/java/ru/vsu/cs/airTrafficControlSystem/dto/PlaneDTO.java
@@ -1,4 +1,18 @@
 package ru.vsu.cs.airTrafficControlSystem.dto;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import ru.vsu.cs.airTrafficControlSystem.models.enums.PlaneType;
+
+@Data
 public class PlaneDTO {
+    @NotNull
+    private PlaneType type;
+
+    @NotNull
+    private String model;
+
+    @NotNull
+    private int capacity;
 }
